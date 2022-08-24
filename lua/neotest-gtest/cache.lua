a3f007123c84782de593618fe40425dc7ea31b46
Alfaix <arsen.kitov@gmail.com>
2022-08-24 22:30:59 +0400

    chore: introduce stable stylua formatting

--- lua/neotest-gtest/cache.lua
+++ lua/neotest-gtest/cache.lua
@@ -15,92 +15,99 @@ local _loaded_caches = {}
 
 -- can't use async.uv.something because the rest of the handler is called in the callback
 local function read_sync(file_path)
-    local file_fd = assert(vim.loop.fs_open(file_path, "r", 438))
-    local stat = assert(vim.loop.fs_fstat(file_fd))
-    local data = assert(vim.loop.fs_read(file_fd, stat.size, 0))
-    assert(vim.loop.fs_close(file_fd))
-    return data
+	local file_fd = assert(vim.loop.fs_open(file_path, "r", 438))
+	local stat = assert(vim.loop.fs_fstat(file_fd))
+	local data = assert(vim.loop.fs_read(file_fd, stat.size, 0))
+	assert(vim.loop.fs_close(file_fd))
+	return data
 end
 
 local cache_path = stddata .. "/neotest-gtest"
 
 function Cache:cache_for(path)
-    local encoded_path = utils.encode_path(path)
-    if _loaded_caches[encoded_path] == nil then
-        local full_path = string.format("%s/%s.json", cache_path, encoded_path)
-        _loaded_caches[encoded_path] = Cache:new(full_path)
-    end
-    return _loaded_caches[encoded_path]
+	local encoded_path = utils.encode_path(path)
+	if _loaded_caches[encoded_path] == nil then
+		local full_path = string.format("%s/%s.json", cache_path, encoded_path)
+		_loaded_caches[encoded_path] = Cache:new(full_path)
+	end
+	return _loaded_caches[encoded_path]
 end
 
 function Cache:new(path)
-    local obj = {_path = path, _data = vim.empty_dict()}
-    setmetatable(obj, {__index = Cache})
+	local obj = { _path = path, _data = vim.empty_dict() }
+	setmetatable(obj, { __index = Cache })
 
-    local exists, _ = utils.fexists(path)
-    if not exists then
-        obj:flush() -- create the file
-    else
-        local json = read_sync(path)
-        obj._data = json == "" and {} or vim.json.decode(json)
-    end
+	local exists, _ = utils.fexists(path)
+	if not exists then
+		obj:flush() -- create the file
+	else
+		local json = read_sync(path)
+		obj._data = json == "" and {} or vim.json.decode(json)
+	end
 
-    return obj
+	return obj
 end
 
-function Cache:data() return self._data end
-function Cache:path() return self._path end
+function Cache:data()
+	return self._data
+end
+function Cache:path()
+	return self._path
+end
 
 function Cache:flush()
-    local as_json = vim.json.encode(self._data)
-    if as_json == nil then return end
-    local file_fd = assert(vim.loop.fs_open(self._path, "w", cache_mode))
-    local bytes = assert(vim.loop.fs_write(file_fd, as_json, 0))
-    assert(bytes == #as_json, bytes)
-    assert(vim.loop.fs_close(file_fd))
+	local as_json = vim.json.encode(self._data)
+	if as_json == nil then
+		return
+	end
+	local file_fd = assert(vim.loop.fs_open(self._path, "w", cache_mode))
+	local bytes = assert(vim.loop.fs_write(file_fd, as_json, 0))
+	assert(bytes == #as_json, bytes)
+	assert(vim.loop.fs_close(file_fd))
 end
 
 local function symlink(path, new_path)
-    if not IS_WINDOWS then vim.loop.fs_symlink(path, new_path) end
-    -- otherwise just don't bother
+	if not IS_WINDOWS then
+		vim.loop.fs_symlink(path, new_path)
+	end
+	-- otherwise just don't bother
 end
 
 function Cache:new_results_dir(opts)
-    opts = vim.tbl_extend("keep", opts or {}, {history_size = 3})
-    local parent_path = Path:new(runs_dir,
-                                 string.format("googletest-of-%s", user_name))
-    parent_path:mkdir({exist_ok = true})
+	opts = vim.tbl_extend("keep", opts or {}, { history_size = 3 })
+	local parent_path = Path:new(runs_dir, string.format("googletest-of-%s", user_name))
+	parent_path:mkdir({ exist_ok = true })
 
-    local existing_paths = {}
-    for _, dir in ipairs(scandir.scan_dir(parent_path.filename,
-                                          {depth = 1, only_dirs = true})) do
-        if dir:match("%/neotest%-gtest%-run%-%d+$") then
-            existing_paths[#existing_paths + 1] = dir
-        end
-    end
+	local existing_paths = {}
+	for _, dir in ipairs(scandir.scan_dir(parent_path.filename, { depth = 1, only_dirs = true })) do
+		if dir:match("%/neotest%-gtest%-run%-%d+$") then
+			existing_paths[#existing_paths + 1] = dir
+		end
+	end
 
-    -- sort newest -> oldest, only leave history_size - 1 files left
-    local path2nr = function(p) return tonumber(p:match([[%d+$]])) end
-    table.sort(existing_paths, function(l, r) return path2nr(l) > path2nr(r) end)
-    for i = opts.history_size, #existing_paths do
-        -- making sure we don't remove something important
-        assert(existing_paths[i]:match(
-            "%/googletest%-of.*%/neotest%-gtest%-run%-%d+$"))
-        Path:new(existing_paths[i]):rm({recusrive = true})
-    end
+	-- sort newest -> oldest, only leave history_size - 1 files left
+	local path2nr = function(p)
+		return tonumber(p:match([[%d+$]]))
+	end
+	table.sort(existing_paths, function(l, r)
+		return path2nr(l) > path2nr(r)
+	end)
+	for i = opts.history_size, #existing_paths do
+		-- making sure we don't remove something important
+		assert(existing_paths[i]:match("%/googletest%-of.*%/neotest%-gtest%-run%-%d+$"))
+		Path:new(existing_paths[i]):rm({ recusrive = true })
+	end
 
-    local new_nr
-    if #existing_paths == 0 then
-        new_nr = 1
-    else
-        new_nr = path2nr(existing_paths[1]) + 1
-    end
-    local new_path = Path:new(parent_path,
-                              ("neotest-gtest-run-%d"):format(new_nr))
-    new_path:mkdir({exist_ok = false})
-    symlink(new_path.filename,
-            Path:new(parent_path, "neotest-gtest-latest").filename)
-    return new_path.filename
+	local new_nr
+	if #existing_paths == 0 then
+		new_nr = 1
+	else
+		new_nr = path2nr(existing_paths[1]) + 1
+	end
+	local new_path = Path:new(parent_path, ("neotest-gtest-run-%d"):format(new_nr))
+	new_path:mkdir({ exist_ok = false })
+	symlink(new_path.filename, Path:new(parent_path, "neotest-gtest-latest").filename)
+	return new_path.filename
 end
 
 return Cache
--- lua/neotest-gtest/runner.lua
+++ lua/neotest-gtest/runner.lua
@@ -26,58 +26,69 @@ M._last_chosen = nil
 ---@return neotest-gtest.Runner|nil the runer for the given path
 ---@return string|nil human-readable error (if any)
 function M.runner_for(path, opts)
-    opts = vim.tbl_extend("keep", opts or {}, {interactive = false})
-    for _, runner in ipairs(M._runners) do
-        if runner:owns(path) then return runner end
-    end
+	opts = vim.tbl_extend("keep", opts or {}, { interactive = false })
+	for _, runner in ipairs(M._runners) do
+		if runner:owns(path) then
+			return runner
+		end
+	end
 
-    if not opts.interactive then return nil end
+	if not opts.interactive then
+		return nil
+	end
 
-    local choice_idx, runner, error
-    if #M._runners ~= 0 then
-        local selected
-        choice_idx, selected, error = ui.select(M._runners, {
-            format = function(option) return option:executable() end,
-            default = M._last_chosen,
-            prompt = string.format(
-                "Choose the executable for %s (or enter a new path)", path),
-            completion = "file",
-            allow_string = true
-        })
-        if error == nil then
-            if choice_idx == nil then
-                -- selected is the path to the test executable entered by the user
-                choice_idx, runner, error =
-                    M.register_runner(selected, nil, {path})
-            else
-                runner = selected
-            end
-        end
-    else
-        local inpt
-        inpt, error = ui.input({
-            prompt = string.format(
-                "Enter the executable path for %s (q or empty cancels): ", path),
-            completion = "file"
-        })
-        if error == nil and inpt then
-            choice_idx, runner, error = M.register_runner(inpt, nil, {path})
-        end
-    end
-    if error then return nil, error end
-    if choice_idx ~= nil then M._last_chosen = choice_idx end
-    return runner, nil
+	local choice_idx, runner, error
+	if #M._runners ~= 0 then
+		local selected
+		choice_idx, selected, error = ui.select(M._runners, {
+			format = function(option)
+				return option:executable()
+			end,
+			default = M._last_chosen,
+			prompt = string.format("Choose the executable for %s (or enter a new path)", path),
+			completion = "file",
+			allow_string = true,
+		})
+		if error == nil then
+			if choice_idx == nil then
+				-- selected is the path to the test executable entered by the user
+				choice_idx, runner, error = M.register_runner(selected, nil, { path })
+			else
+				runner = selected
+			end
+		end
+	else
+		local inpt
+		inpt, error = ui.input({
+			prompt = string.format("Enter the executable path for %s (q or empty cancels): ", path),
+			completion = "file",
+		})
+		if error == nil and inpt then
+			choice_idx, runner, error = M.register_runner(inpt, nil, { path })
+		end
+	end
+	if error then
+		return nil, error
+	end
+	if choice_idx ~= nil then
+		M._last_chosen = choice_idx
+	end
+	return runner, nil
 end
 
 function M.select_runner(index, exe_path)
-    if index ~= nil then return M._runners[index] end
-    if exe_path ~= nil then
-        exe_path = utils.normalize_path(exe_path)
-        for _, runner in ipairs(M._runners) do
-            if runner:executable() == exe_path then return runner end
-        end
-    end
-    return nil
+	if index ~= nil then
+		return M._runners[index]
+	end
+	if exe_path ~= nil then
+		exe_path = utils.normalize_path(exe_path)
+		for _, runner in ipairs(M._runners) do
+			if runner:executable() == exe_path then
+				return runner
+			end
+		end
+	end
+	return nil
 end
 
 ---Loads runners from an object previously saved with dump_runners().
@@ -87,29 +98,28 @@ end
 ---@returns boolean ok/not ok
 ---@returns string|nil error message if any
 function M.load_runners(data, opts)
-    local err
-    opts = vim.tbl_extend("keep", opts or {},
-                          {clear = true, on_error = "notify"})
-    local runners = {}
-    for _, runner_info in ipairs(data) do
-        runners[#runners + 1], err = Runner:new(runner_info.executable_path,
-                                                runner_info.compile_command,
-                                                runner_info.paths)
-        if opts.on_error == "propagate" then
-            return false, err
-        elseif opts.on_error == "notify" then
-            vim.notify(string.format("Error loading executable at %s: %s",
-                                     runner_info.executable_path or "<nil>", err),
-                       3)
-        end
-    end
+	local err
+	opts = vim.tbl_extend("keep", opts or {}, { clear = true, on_error = "notify" })
+	local runners = {}
+	for _, runner_info in ipairs(data) do
+		runners[#runners + 1], err =
+			Runner:new(runner_info.executable_path, runner_info.compile_command, runner_info.paths)
+		if opts.on_error == "propagate" then
+			return false, err
+		elseif opts.on_error == "notify" then
+			vim.notify(
+				string.format("Error loading executable at %s: %s", runner_info.executable_path or "<nil>", err),
+				3
+			)
+		end
+	end
 
-    if opts.clear then
-        M._runners = runners
-    else
-        M._runners = vim.tbl_flatten({M._runners, runners})
-    end
-    return true
+	if opts.clear then
+		M._runners = runners
+	else
+		M._runners = vim.tbl_flatten({ M._runners, runners })
+	end
+	return true
 end
 
 ---Returns json-friendly list of registered runners. If `include_unused` is false,
@@ -117,62 +127,75 @@ end
 ---@param opts table Options, default: {include_unused = false}
 ---@return neotest-gtest.RunnerInfo[]
 function M.dump_runners(opts)
-    opts = vim.tbl_extend("keep", opts or {}, {include_unused = false})
-    local runners = {}
-    for _, runner in ipairs(M._runners) do
-        if opts.include_unused or runner:is_used() then
-            runners[#runners + 1] = {
-                executable_path = runner._run_command,
-                compile_command = runner._compile_command,
-                paths = runner._paths
-            }
-        end
-    end
-    return runners
+	opts = vim.tbl_extend("keep", opts or {}, { include_unused = false })
+	local runners = {}
+	for _, runner in ipairs(M._runners) do
+		if opts.include_unused or runner:is_used() then
+			runners[#runners + 1] = {
+				executable_path = runner._run_command,
+				compile_command = runner._compile_command,
+				paths = runner._paths,
+			}
+		end
+	end
+	return runners
 end
 
 function M.register_runner(executable_path, compile_command, paths)
-    executable_path = utils.normalize_path(executable_path)
-    for i, runner in ipairs(M._runners) do
-        if runner:executable() == executable_path then
-            for _, path in ipairs(paths) do runner:add_path(path) end
-            if compile_command then
-                runner._compile_command = compile_command
-            end
-            return i, runner
-        end
-    end
+	executable_path = utils.normalize_path(executable_path)
+	for i, runner in ipairs(M._runners) do
+		if runner:executable() == executable_path then
+			for _, path in ipairs(paths) do
+				runner:add_path(path)
+			end
+			if compile_command then
+				runner._compile_command = compile_command
+			end
+			return i, runner
+		end
+	end
 
-    local runner, err = Runner:new(executable_path, compile_command, paths)
-    if runner == nil then return nil, nil, err end
-    local new_idx = #M._runners + 1
-    M._runners[new_idx] = runner
-    return new_idx, runner, nil
+	local runner, err = Runner:new(executable_path, compile_command, paths)
+	if runner == nil then
+		return nil, nil, err
+	end
+	local new_idx = #M._runners + 1
+	M._runners[new_idx] = runner
+	return new_idx, runner, nil
 end
 
 function Runner:new(executable_path, compile_command, paths)
-    local runner = {_compile_command = compile_command, _paths = {}}
-    setmetatable(runner, {__index = Runner})
-    local ok, err = runner:set_executable(executable_path)
-    if not ok then return nil, err end
-    for _, path in ipairs(paths) do runner:add_path(path) end
-    return runner, nil
+	local runner = { _compile_command = compile_command, _paths = {} }
+	setmetatable(runner, { __index = Runner })
+	local ok, err = runner:set_executable(executable_path)
+	if not ok then
+		return nil, err
+	end
+	for _, path in ipairs(paths) do
+		runner:add_path(path)
+	end
+	return runner, nil
 end
 
-function Runner:executable() return self._executable_path end
-function Runner:set_executable(executable_path)
-    executable_path = utils.normalize_path(executable_path)
-    local exists, err = utils.fexists(executable_path)
-    if not exists then
-        -- could also be permission denied, print the error
-        return false, string.format("Cannot find an executable at path %s: %s",
-                                    executable_path, err)
-    end
-    self._executable_path = executable_path
-    return true, nil
+function Runner:executable()
+	return self._executable_path
 end
-function Runner:compile_command() return self._compile_command end
-function Runner:set_compile_command(command) self._compile_command = command end
+function Runner:set_executable(executable_path)
+	executable_path = utils.normalize_path(executable_path)
+	local exists, err = utils.fexists(executable_path)
+	if not exists then
+		-- could also be permission denied, print the error
+		return false, string.format("Cannot find an executable at path %s: %s", executable_path, err)
+	end
+	self._executable_path = executable_path
+	return true, nil
+end
+function Runner:compile_command()
+	return self._compile_command
+end
+function Runner:set_compile_command(command)
+	self._compile_command = command
+end
 
 ---Returns true if parent is equal to child or parent is a parent directory of child.
 ---Assumes both paths exist, are absolute and are normalized.
@@ -180,50 +203,57 @@ function Runner:set_compile_command(command) self._compile_command = command end
 ---@param child string
 ---@return boolean
 local function _is_parent(parent, child)
-    local is_prefix = vim.startswith(child, parent)
-    return is_prefix and
-               (#parent == #child or child:sub(#parent + 1, #parent + 1) ==
-                   Path.path.sep)
+	local is_prefix = vim.startswith(child, parent)
+	return is_prefix and (#parent == #child or child:sub(#parent + 1, #parent + 1) == Path.path.sep)
 end
 
 function Runner:owns(path)
-    path = utils.normalize_path(path)
-    for _, owned_path in ipairs(self._paths) do
-        if _is_parent(owned_path, path) then return true end
-    end
-    return false
+	path = utils.normalize_path(path)
+	for _, owned_path in ipairs(self._paths) do
+		if _is_parent(owned_path, path) then
+			return true
+		end
+	end
+	return false
 end
 
 function Runner:configure()
-    local fields = {
-        {
-            name = "executable",
-            human_name = "the path to the test executable",
-            default = self._executable_path,
-            required = true,
-            completion = "file"
-        }, {
-            name = "compile_command",
-            human_name = "the compilation command",
-            default = self._compile_command,
-            required = false,
-            completion = "file"
-        }, {
-            name = "paths",
-            human_name = "test paths (files/directories)",
-            default = table.concat(vim.tbl_map(function(path)
-                return path:find("%s") and string.format("'%s'", path) or path
-            end, self._paths), " "),
-            required = true,
-            completion = "file"
-        }
-    }
-    local user_input = ui.configure(fields)
-    self:set_executable(user_input.executable)
-    local paths = utils.parse_words(user_input.paths)
-    for _, path in ipairs(paths) do self:add_path(path) end
-    self:set_compile_command(user_input.compile_command)
-    return true, nil
+	local fields = {
+		{
+			name = "executable",
+			human_name = "the path to the test executable",
+			default = self._executable_path,
+			required = true,
+			completion = "file",
+		},
+		{
+			name = "compile_command",
+			human_name = "the compilation command",
+			default = self._compile_command,
+			required = false,
+			completion = "file",
+		},
+		{
+			name = "paths",
+			human_name = "test paths (files/directories)",
+			default = table.concat(
+				vim.tbl_map(function(path)
+					return path:find("%s") and string.format("'%s'", path) or path
+				end, self._paths),
+				" "
+			),
+			required = true,
+			completion = "file",
+		},
+	}
+	local user_input = ui.configure(fields)
+	self:set_executable(user_input.executable)
+	local paths = utils.parse_words(user_input.paths)
+	for _, path in ipairs(paths) do
+		self:add_path(path)
+	end
+	self:set_compile_command(user_input.compile_command)
+	return true, nil
 end
 
 ---Adds path to the owned paths list. It is guaranteed Runner:owns(path) will
@@ -231,50 +261,56 @@ end
 ---already owned.
 ---@param path string
 function Runner:add_path(path)
-    path = utils.normalize_path(path)
-    for _, owned_path in ipairs(self._paths) do
-        -- already owned: do nothing
-        if _is_parent(owned_path, path) then return end
-    end
-    local children = {}
-    for i, owned_path in ipairs(self._paths) do
-        if _is_parent(path, owned_path) then children[i] = true end
-    end
-    if vim.tbl_isempty(children) then
-        self._paths[#self._paths + 1] = path
-    else
-        local new_paths = {}
-        for i, owned_path in ipairs(self._paths) do
-            if not children[i] then
-                new_paths[#new_paths + 1] = owned_path
-            end
-        end
-        new_paths[#new_paths + 1] = path
-        self._paths = new_paths
-    end
+	path = utils.normalize_path(path)
+	for _, owned_path in ipairs(self._paths) do
+		-- already owned: do nothing
+		if _is_parent(owned_path, path) then
+			return
+		end
+	end
+	local children = {}
+	for i, owned_path in ipairs(self._paths) do
+		if _is_parent(path, owned_path) then
+			children[i] = true
+		end
+	end
+	if vim.tbl_isempty(children) then
+		self._paths[#self._paths + 1] = path
+	else
+		local new_paths = {}
+		for i, owned_path in ipairs(self._paths) do
+			if not children[i] then
+				new_paths[#new_paths + 1] = owned_path
+			end
+		end
+		new_paths[#new_paths + 1] = path
+		self._paths = new_paths
+	end
 end
 
 -- TODO
 function Runner:recompile(opts) end
 
 function Runner:is_used()
-    for _, owned_path in ipairs(self._paths) do
-        local pathobj = Path:new(owned_path)
-        if pathobj:exists() then
-            if pathobj:is_dir() then
-                local test_files = scandir.scan_dir(pathobj, {
-                    add_dirs = false,
-                    respect_gitignore = false,
-                    search_pattern = utils.is_test_file,
-                    silent = true
-                })
-                if #test_files ~= 0 then return true end
-            elseif utils.is_test_file(pathobj.filename) then
-                return true
-            end
-        end
-    end
-    return false
+	for _, owned_path in ipairs(self._paths) do
+		local pathobj = Path:new(owned_path)
+		if pathobj:exists() then
+			if pathobj:is_dir() then
+				local test_files = scandir.scan_dir(pathobj, {
+					add_dirs = false,
+					respect_gitignore = false,
+					search_pattern = utils.is_test_file,
+					silent = true,
+				})
+				if #test_files ~= 0 then
+					return true
+				end
+			elseif utils.is_test_file(pathobj.filename) then
+				return true
+			end
+		end
+	end
+	return false
 end
 
 return M
