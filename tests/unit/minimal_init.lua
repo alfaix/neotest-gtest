641dcafc09d8085b3b4d2762f4e8b0ac637cc982
Arsen Kitov <arsen.kitov@gmail.com>
2025-06-04 16:30:18 +0200

    Fix test env setup

--- tests/unit/minimal_init.lua
+++ tests/unit/minimal_init.lua
@@ -6,5 +6,13 @@ vim.notify = print
 vim.opt.swapfile = false
 vim.opt.rtp:append(".")
 for _, plugin in ipairs(plugins) do
-  vim.opt.rtp:append(lazypath .. "/" .. plugin)
+  local plugin_path = lazypath .. "/" .. plugin
+  vim.opt.rtp:append(plugin_path)
+  package.path = package.path
+    .. ";"
+    .. plugin_path
+    .. "/lua/?.lua"
+    .. ";"
+    .. plugin_path
+    .. "/lua/?/init.lua"
 end
